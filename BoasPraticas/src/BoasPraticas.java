58a50ab407c2c991e1cb1d4c8e0ba8e5f18b181a
deiseguth <deiiseguth@gmail.com>
2023-01-04 20:44:30 -0300

    01

---
+++ BoasPraticas/src/BoasPraticas.java
@@ -0,0 +1,12 @@
+public class BoasPraticas {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        /*
+         * Switch é para valores exatos e if para expressões booleanas 
+         * Evitar usar o defalt do switch para "cases genericos". Ele deve ser usado para casos inválidos ou exceções.
+         * Evitar o efeito "flecha" dos if's
+         * Evitar muitos if's aninhados
+         * Diminuir o tamanho dos if's
+         */
+    }
+}
---
+++ LogicaCondicional/src/LogicaCondicional.java
@@ -0,0 +1,25 @@
+public class LogicaCondicional {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+/*
+ * ^ "xor" que indica opostos
+ * 
+ * Evitar operadores ternários: emMovimento ? freia : null
+ * condicao ? true : false
+ * ligado ? desligar : ligar
+ * 
+ * switch que dá opções (escolhas) e dá os cases e/ou breaks
+ * 
+ * switch (olhos){
+ * case "AZUIS" :
+ * break;
+ * case "VERDES" :
+ * break;
+ * case "CASTANHOS" :
+ * break;
+ * default:
+ * break;
+ * }
+ */
